71407c10cc4dd1eabab6c1e29d8fd46a23ea845b
ShailMurmu <shailmurmu1996@gmail.com>
2017-11-11 00:18:26 +0530

    Resolved deprecated mongoDB connect warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{autoReconnect: true}).then((result) => {
+mongoose.connect('mongodb://localhost:27017/TodoApp',{
+    useMongoClient: true,
+    poolSize: 2,
+    promiseLibrary: global.Promise
+}).then((result) => {
     console.log('database connection established');
 }, (err) => {
     console.log('Unable to connect database');
